db9ae646678b8f0d4ace90ac785d8efc7b5cc15d
brayden-weimholt <bweimhol@uci.edu>
2025-03-15 03:52:57 +0000

    Dijkstras algorithm implemented; maps a vertex's integer representation to it's distance (shortest possible from the Graph) from the single-source node. Returns this vector of distances, update the previous vector as well when you move through to map that vertex back to the start.

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,7 +1,38 @@
 #include "dijkstras.h"
 
 
-vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous);
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous){
+    int n = G.numVertices;
+    vector<int> distances(n, INF);
+    previous.assign(n, -1);
+    vector<bool> visited(n, false);
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, source});
+    if(source < n) distances[source] = 0;
+    else source = 0;
+
+    while(!pq.empty()){
+        int u = pq.top().second;
+
+        if(visited[u]) continue;
+        else visited[u] = true;
+
+        int dist_u = pq.top().first;
+        pq.pop();
+
+        for(const Edge& e : G[u]){
+            int v = e.dst;
+            int wgt = e.weight;
+            if(!visited[v] && distances[u] + wgt < distances[v]){
+                distances[v] = distances[u] + wgt;
+                previous[v] = u;
+                pq.push({distances[v], v});
+            }
+        }
+    }
+    return distances;
+}
 
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination);
 
