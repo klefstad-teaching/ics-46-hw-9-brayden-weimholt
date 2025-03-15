5c9d2d644ec61328217bcc183770a1aa70a3e656
brayden-weimholt <bweimhol@uci.edu>
2025-03-15 04:14:41 +0000

    Main runs the program for dijkstra's algorithm by accepting a file path name, attempting to input it into a graph. Then, run the algo on the graph, getting the distances vector back. Iterate through the vertices, extracting its path by checking its distance for validitiy (less than infinity) and deepening thru to its parent. Print the path and return.

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -1,3 +1,18 @@
 #include "dijkstras.h"
 
-int main(){return 0;}
+int main(int argc, char* argv[]){
+    Graph G;
+    string file;
+    if(argc >= 1) file = argv[0];
+    else file = "small.txt";
+    file_to_graph(file, G);
+
+    int source = 0;
+    vector<int> previous;
+    vector<int> distances = dijkstra_shortest_path(G, source, previous);
+    for(int i = 0; i < G.numVertices; ++i){
+        vector<int> path = extract_shortest_path(distances, previous, i);
+        print_path(path, distances[i]);
+    }
+    return 0;
+}
