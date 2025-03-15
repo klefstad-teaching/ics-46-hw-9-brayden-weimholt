185d090614615831164248e9c6fa490abad87a02
brayden-weimholt <bweimhol@uci.edu>
2025-03-15 02:37:56 +0000

    Verify_word_ladder copied in-now will create main and try it.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -65,10 +65,48 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
     return {};
 }
 
-void load_words(set<string> & word_list, const string& file_name);
+void load_words(set<string> & word_list, const string& file_name){
+    ifstream in(file_name);
+    if(!in.is_open()) error("", "", "Input file incorrectly named.");
+    string word;
+    while(in >> word){
+        transform(word.begin(), word.end(), word.begin(), ::tolower);
+        word_list.insert(word);
+    }
+}
 
-void print_word_ladder(const vector<string>& ladder);
+void print_word_ladder(const vector<string>& ladder){
+    if(ladder.empty()) error("", "", "No word ladder found.");
+    
+    cout << "Word ladder found (" << ladder.size() - 1 << " steps):" << endl;
+    for (size_t i = 0; i < ladder.size(); ++i) {
+        if (i > 0) {
+            cout << " → ";
+        }
+        cout << ladder[i];
+    }
+    cout << endl;
+}
 
-void verify_word_ladder();
+#define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
+void verify_word_ladder() {
+
+    set<string> word_list;
+
+    load_words(word_list, "words.txt");
+
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+
+}
 
 
