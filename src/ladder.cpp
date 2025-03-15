24fe377de5df9b10ce9de13f7e3c2ea932c30f20
brayden-weimholt <bweimhol@uci.edu>
2025-03-15 01:14:23 +0000

    Potential rough copy of the write-up's generation of the correct word-ladder (or return of nothing if impossible) implemented, need to try and make is_adjacent work. How edit_distance_within works is beyong me. Will do that, then find out how to load the words, print, and then copy the verification, run it in main, move to Dijkstras and repeat.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -9,7 +9,34 @@ bool edit_distance_within(const std::string& str1, const std::string& str2, int 
 
 bool is_adjacent(const string& word1, const string& word2);
 
-vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list);
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list){
+    queue<vector<string>> ladder_queue;
+    ladder_queue.push({begin_word});
+
+    set<string> visited;
+    visited.insert(begin_word);
+
+    while (!ladder_queue.empty()) {
+        vector<string> current_ladder = ladder_queue.front();
+        ladder_queue.pop();
+        string last_word = current_ladder.back();
+
+        for (const string& word : word_list) {
+            if (is_adjacent(last_word, word) && !visited.count(word)) {
+                visited.insert(word);
+                vector<string> new_ladder = current_ladder;
+                new_ladder.push_back(word);
+
+                if (word == end_word) {
+                    return new_ladder;
+                }
+
+                ladder_queue.push(new_ladder);
+            }
+        }
+    }
+    return {};
+}
 
 void load_words(set<string> & word_list, const string& file_name);
 
